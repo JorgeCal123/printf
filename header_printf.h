f0e2d72a441f82ac85736da7eff95cb08a359093
JorgeCal123/boom.git <git@github.com>
2021-11-07 13:58:20 -0800

    prototipos en el header del archivo _printf

--- header_printf.h
+++ header_printf.h
@@ -2,4 +2,8 @@
 #define HEADER PRINTF_H
 #include <stdarg.h>
 int _printf(const char *format, ...);
+void is_Naturally_Special_Character(va_list list);
+void is_format_Specificator(va_list list);
+void is_alternately_Special_Characters(va_list list);
+void is_flag(va_list list);
 #endif
