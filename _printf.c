42d37b6de2bc9b7b442b53e844d92581f7889d9b
JorgeCal123/boom.git <git@github.com>
2021-11-07 15:41:23 -0800

    cambios dentro del _printf el header y el main

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
-#include "header_printf"
+#include "main.h"
+#include <stdio.h>
 /**
  * _printf - description
  * @format - constant pointer type char
@@ -6,4 +7,37 @@
  */
 int _printf(const char *format, ...)
 {
+	va_list list;
+	const char *p;
+
+	va_start(list, format);
+	for (p = format; *p; p++)
+	{
+	/*is_Naturally_Special_Characters(list);*/
+		if(*p == '%')
+            {
+		    printf("iporcentaje\n");
+		    is_Format_Specificator(p, list);
+                putchar(*p);
+                continue;
+            }
+
+	}
+	va_end(list);
+	return (0);
+}
+
+void is_Format_Specificator(__attribute__((unused))const char *p, va_list list)
+{
+	is_Flag(list);
+
+	printf("funcion ormato\n");
+
+
+}
+
+
+void is_Flag(__attribute__((unused))va_list list)
+{
+	printf("entra flag\n");
 }
--- main.h
+++ main.h
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 int _printf(const char *format, ...);
 void is_Naturally_Special_Character(va_list list);
-void is_format_Specificator(va_list list);
-void is_alternately_Special_Characters(va_list list);
-void is_flag(va_list list);
+void is_Format_Specificator(const char* p, va_list list);
+void is_Alternately_Special_Characters(va_list list);
+void is_Flag(va_list list);
 #endif
